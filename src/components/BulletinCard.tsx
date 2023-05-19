c77e80636fc37cf69d76e9169c8b6a8641b38a75
Jen <quanistcoding@gmail.com>
2023-05-19 12:30:07 +0800

    Display all bulletins

--- src/components/BulletinCard.tsx
+++ src/components/BulletinCard.tsx
@@ -1,18 +1,29 @@
-import { Card, CardBody, CardHeader, HStack, Text } from "@chakra-ui/react";
-import React from "react";
+import { Card, CardBody, HStack, Text } from "@chakra-ui/react";
+import Bulletin from "../entities/bulletin";
 
-const BulletinCard = () => {
+interface Props {
+  bulletin: Bulletin;
+}
+
+const BulletinCard = ({ bulletin }: Props) => {
+  const date =
+    new Date(bulletin?.date).getMonth() +
+    "月" +
+    new Date(bulletin?.date).getDate() +
+    "日";
   return (
     <Card borderColor={"gray.400"} borderWidth={1}>
       <CardBody>
         <HStack justifyContent={"space-between"}>
-          <Text>發佈者:</Text>
-          <Text>2022-03-20</Text>
+          <Text>發佈者: {bulletin?.author?.name}</Text>
+          <Text>{date}</Text>
         </HStack>
       </CardBody>
-      <CardBody as="h1">標題</CardBody>
       <CardBody>
-        <Text>內容</Text>
+        <Text>標題：{bulletin?.title}</Text>
+      </CardBody>
+      <CardBody>
+        <Text>內容：{bulletin?.content}</Text>
       </CardBody>
     </Card>
   );
--- src/pages/ButtetinPage.tsx
+++ src/pages/ButtetinPage.tsx
@@ -1,15 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Button, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import BulletinCard from "../components/BulletinCard";
 import { Link } from "react-router-dom";
+import useBulletins from "../hooks/useBulletins";
 
 const ButtetinPage = () => {
+  const { bulletins } = useBulletins();
   return (
     <>
       <Link to="/bulletin/add">
         <Button marginBottom={2}>新增公告</Button>
       </Link>
-      <BulletinCard />
+      <SimpleGrid
+        columns={{
+          base: 1,
+          sm: 2,
+          md: 3,
+        }}
+        spacing={5}
+      >
+        {bulletins?.map((b, index) => (
+          <BulletinCard bulletin={b} key={index} />
+        ))}
+      </SimpleGrid>
     </>
   );
 };
