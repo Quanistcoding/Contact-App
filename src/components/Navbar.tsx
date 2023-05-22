b1d919959c08ac95cb0f60e91d25069fea0c35cd
Jen <quanistcoding@gmail.com>
2023-05-22 14:53:42 +0800

    Hide serch bar if not on contact list page.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 import { Box, HStack, Text } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SearchInput from "./SearchInput";
 import useAuth from "../providers/authProvider/useAuth";
 
 const Navbar = () => {
   const { authUser } = useAuth();
+  const location = useLocation();
+  console.log(location);
   return (
-    <HStack padding={1} bg="gray.700" justifyContent={"space-between"}>
+    <HStack
+      padding={1}
+      bg="gray.700"
+      justifyContent={"space-between"}
+      height={"50px"}
+    >
       <Box whiteSpace={"nowrap"}>
         <Link to="/">
           <Text
@@ -20,7 +27,7 @@ const Navbar = () => {
           </Text>
         </Link>
       </Box>
-      {authUser && <SearchInput />}
+      {authUser && location.pathname === "/" && <SearchInput />}
       <ColorModeSwitch />
     </HStack>
   );
